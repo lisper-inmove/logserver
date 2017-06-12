d5a75329e0b3debdb07a3e9febdd843cced8654a
inmove <lisper.inmove@gmail.com>
2017-06-12 10:11:58 +0800

    done

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import json
 if "__main__" == __name__:
     for x in xrange(1, 2):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-        sock.connect(('127.0.0.1', 8003));
+        sock.connect(('127.0.0.1', 6103));
         data = [[1, 1002, "2015-01-01", "192.168.10.219", "this is a test"]]
         sock.send(json.dumps(data));
 
